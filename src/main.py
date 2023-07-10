999bb989a3fb0c4237ba78a1ce93c6e2e209dd49
Ryan Rennoir <ryanrennoir9@gmail.com>
2023-07-10 11:41:31 +0200

    Fix bool cast and add helper for int

--- src/main.py
+++ src/main.py
@@ -1,19 +1,39 @@
 import os
 from portainer import PortainerStack
 
+
+def get_bool_env(env_name: str) -> bool:
+
+    input = os.environ[env_name].lower()
+
+    if input not in ["true", "false"]:
+        raise ValueError("Invalid argument for bool type, must be true or false")
+
+    return input == "true"
+
+def get_int_env(env_name: str) -> int:
+
+    input = os.environ[env_name]
+
+    if not input.isnumeric():
+        raise ValueError("Invalid argument for int type, must be a number")
+
+    return int(input)
+
+
 def main():
     
     stackInfo = PortainerStack(os.environ["INPUT_PORTAINERURL"],
                                os.environ["INPUT_PORTAINERUSERNAME"],
                                os.environ["INPUT_PORTAINERPASSWORD"],
-                               int(os.environ["INPUT_PORTAINERENDPOINTID"]),
+                               get_int_env("INPUT_PORTAINERENDPOINTID"),
                                os.environ["INPUT_STACKNAME"], 
-                               int(os.environ["INPUT_STACKTYPE"]),
+                               get_int_env("INPUT_STACKTYPE"),
                                os.environ["INPUT_COMPOSEFILE"],
-                               bool(os.environ["INPUT_PRUNE"]),
-                               bool(os.environ["INPUT_PULLIMAGE"]),
-                               bool(os.environ["INPUT_DELETE"]),
-                               bool(os.environ["INPUT_VERIFYSSL"]))
+                               get_bool_env("INPUT_PRUNE"),
+                               get_bool_env("INPUT_PULLIMAGE"),
+                               get_bool_env("INPUT_DELETE"),
+                               get_bool_env("INPUT_VERIFYSSL"))
 
     stackInfo.deployStack()
 
